be92a45c62f4c6d33a47c99e406097a7c67178e0
GuyMiNgabirano <ngabiranog8@gmail.com>
2023-05-05 12:24:38 +0200

    addmain2

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
-print("HelloWord")
+#x = "Python est magique"
+#print(x)
+#def myfunction(nom):
+#   print("Bonjour " +nom)
+if __name__ == '__main__':
+    def myfunction(nom):
+        print("Bonjour " +nom)
+    myfunction("Guy")
+
+   #x,y,z=1,2,3
+   #print(x)
+   #print(y)
+   #print(z)"""
+   #print("Hello class")
+   #a = 4
+   #b = 6
+   #c = "Bonjour"
+   #d = " Tout le monde" 
+   # Les operateurs d'affectation
+   #print(10+5)
+   #print(10-5)
+   #print(10*5)
+   #print(10/5)
+   #print(10%3)
+   #print(5**2)
+   # Les operateurs de comparaison
+   # inferieur ou égal : <=
+   # superieur ou égal : >=
+   # inferieur : <
+   # superieur : >
+   # égal : ==
+   # different : !=
+   #print(a+b)
+   #print(c+d)
+   #x = "Bonjour"
+   #x = "L\'arbre"
+   #print(x)
+   #print(len(x))
+   #print(x[3])
+   #for a in x:
+       #print(a)
+"""salutation = "Bonjour"
+for s in salutation :
+    print(s)
+
+a,b = 5,10  
+
+if a == b or a > 7:
+    print(a)
+elif a < b :
+    print(a)
+else :
+    print(b)"""
+"""i = 1
+while i < 10:
+    i+=1
+    if i==5 :
+        continue
+    print(i)
+    #break
+    #i += 1"""
